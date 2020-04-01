c285b2b9850181b46ef5a2cc4bbd79e02a2d66ec
Derek Schauland <derekschauland@users.noreply.github.com>
2020-04-01 14:25:36 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "test" {
+  name     = "my-resources"
+  location = "West Europe"
+}
+
 module "network" {
   source  = "app.terraform.io/derek2-training/network/azurerm"
   version = "3.0.1"
